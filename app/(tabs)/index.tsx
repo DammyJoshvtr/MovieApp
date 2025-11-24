98d3f044370d7e4262dc43493d53dfc62ffee2cf
Damilola Joshua <damilolajoshua2021@gmail.com>
2025-11-24 13:55:56 +0100

    created a custom hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
+import {images} from "@/constants/images";
+import { icons } from "@/constants/icons"
 import { Link } from 'expo-router'
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-500 text-center font-bold">
-          Welcome To my Movie App!
-      </Text>
-        <Link href="/onboarding">Onboarding</Link>
-        <Link href="/movie/avengers">Avengers Movie</Link>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="w-full absolute z-0" />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10
+        }}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <View>
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movies"
+          />
+        </View>
+      </ScrollView>
     </View>
+
   );
 }
